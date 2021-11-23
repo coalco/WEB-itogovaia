5ff4b7464c3dfad0a320157ddb01adb4818227e1
coalco <94670879+coalco@users.noreply.github.com>
2021-11-23 12:33:29 +0600

    Delete Computer_center directory

--- Computer_center/vendor/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-$connect = new mysqli("localhost", "root", "", "logindata");
-if (!$connect) {
-    die("Error connect to DataBase");
-};
-?>
